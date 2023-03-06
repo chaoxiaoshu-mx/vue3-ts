cc6f26d5e6db0dcd35961ad912898ddb2993bd9d
chaoxiaoshu-mx <leukotrichia@163.com>
2023-03-06 17:36:16 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
  * @Author: chaoxiaoshu-mx leukotrichia@163.com
  * @Date: 2023-03-05 18:42:45
  * @LastEditors: chaoxiaoshu-mx leukotrichia@163.com
- * @LastEditTime: 2023-03-06 17:28:47
+ * @LastEditTime: 2023-03-06 17:36:11
  * @FilePath: \my-vue-app\src\components\HelloWorld.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -21,7 +21,7 @@ const count = ref(0)
     <button type="button" @click="count++">次数</button>
     <p>
       编辑 
-      <code>components/HelloWorld.vue</code> to test HMR
+      <code>components/HelloWorld.vue</code> to test HMR 
     </p>
   </div>
 
